c0ba46202c2ebdb02c57927c4003d046aedd8bbf
Maxim Piskunov <felreaverbrah@gmail.com>
2022-01-16 22:41:13 +0300

    Eval: let statements, functions, closures

--- repl/repl.go
+++ repl/repl.go
@@ -1,49 +1,51 @@
 package repl
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "github.com/Soj447/gonk/lexer"
-    "github.com/Soj447/gonk/parser"
-    "github.com/Soj447/gonk/evaluator"
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/Soj447/gonk/evaluator"
+	"github.com/Soj447/gonk/lexer"
+	"github.com/Soj447/gonk/object"
+	"github.com/Soj447/gonk/parser"
 )
 
 const PS1 = "~>"
 
 func Start(in io.Reader, out io.Writer) {
-    scanner := bufio.NewScanner(in)
+	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
-    for {
-        fmt.Printf(PS1)
-        scanned := scanner.Scan()
-        if !scanned {
-            return
-        }
+	for {
+		fmt.Printf(PS1)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
 
-        line := scanner.Text()
-        l := lexer.New(line)
-        p := parser.New(l)
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
 
-        program := p.ParseProgram()
-        if len(p.Errors()) != 0 {
-            printParserErrors(out, p.Errors())
-            continue
-        }
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
 
-        evaluated := evaluator.Eval(program)
-        if evaluated != nil {
-            io.WriteString(out, evaluated.Inspect())
-            io.WriteString(out, "\n")
-        }
-        
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 
-    }
+	}
 }
 
 func printParserErrors(out io.Writer, errors []string) {
-    io.WriteString(out, "parser errors:\n")
-    for _, msg := range errors {
-        io.WriteString(out, "\t" + msg + "\n")
-    }
+	io.WriteString(out, "parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
